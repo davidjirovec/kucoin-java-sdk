fc2ce14f0a015a6ffea8be568461959aa59f77af
David Jirovec <david.jirovec@gmail.com>
2021-06-05 14:54:43 +0200

    Try orderbook auth

--- src/main/java/com/kucoin/sdk/KucoinClientBuilder.java
+++ src/main/java/com/kucoin/sdk/KucoinClientBuilder.java
@@ -3,20 +3,46 @@
  */
 package com.kucoin.sdk;
 
-import java.io.IOException;
-
-import com.kucoin.sdk.rest.adapter.*;
-import com.kucoin.sdk.rest.interfaces.*;
-import org.apache.commons.lang3.StringUtils;
-
 import com.kucoin.sdk.constants.APIConstants;
 import com.kucoin.sdk.impl.KucoinPrivateWSClientImpl;
 import com.kucoin.sdk.impl.KucoinPublicWSClientImpl;
 import com.kucoin.sdk.impl.KucoinRestClientImpl;
+import com.kucoin.sdk.rest.adapter.AccountAPIAdapter;
+import com.kucoin.sdk.rest.adapter.CommonAPIAdapter;
+import com.kucoin.sdk.rest.adapter.CurrencyAPIAdaptor;
+import com.kucoin.sdk.rest.adapter.DepositAPIAdapter;
+import com.kucoin.sdk.rest.adapter.FillAPIAdapter;
+import com.kucoin.sdk.rest.adapter.HistoryAPIAdapter;
+import com.kucoin.sdk.rest.adapter.LoanAPIAdapter;
+import com.kucoin.sdk.rest.adapter.MarginAPIAdapter;
+import com.kucoin.sdk.rest.adapter.OrderAPIAdapter;
+import com.kucoin.sdk.rest.adapter.OrderBookAPIAdapter;
+import com.kucoin.sdk.rest.adapter.StopOrderAPIAdapter;
+import com.kucoin.sdk.rest.adapter.SymbolAPIAdaptor;
+import com.kucoin.sdk.rest.adapter.TimeAPIAdapter;
+import com.kucoin.sdk.rest.adapter.UserAPIAdapter;
+import com.kucoin.sdk.rest.adapter.WithdrawalAPIAdapter;
+import com.kucoin.sdk.rest.interfaces.AccountAPI;
+import com.kucoin.sdk.rest.interfaces.CommonAPI;
+import com.kucoin.sdk.rest.interfaces.CurrencyAPI;
+import com.kucoin.sdk.rest.interfaces.DepositAPI;
+import com.kucoin.sdk.rest.interfaces.FillAPI;
+import com.kucoin.sdk.rest.interfaces.HistoryAPI;
+import com.kucoin.sdk.rest.interfaces.LoanAPI;
+import com.kucoin.sdk.rest.interfaces.MarginAPI;
+import com.kucoin.sdk.rest.interfaces.OrderAPI;
+import com.kucoin.sdk.rest.interfaces.OrderBookAPI;
+import com.kucoin.sdk.rest.interfaces.StopOrderAPI;
+import com.kucoin.sdk.rest.interfaces.SymbolAPI;
+import com.kucoin.sdk.rest.interfaces.TimeAPI;
+import com.kucoin.sdk.rest.interfaces.UserAPI;
+import com.kucoin.sdk.rest.interfaces.WithdrawalAPI;
 import com.kucoin.sdk.websocket.ChooseServerStrategy;
 import com.kucoin.sdk.websocket.RandomChooseStrategy;
-
 import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
 
 /**
  * Created by chenshiwei on 2019/1/9.
@@ -84,7 +110,7 @@ public class KucoinClientBuilder {
         if (timeAPI == null) timeAPI = new TimeAPIAdapter(baseUrl);
         if (commonAPI == null) commonAPI = new CommonAPIAdapter(baseUrl);
         if (symbolAPI == null) symbolAPI = new SymbolAPIAdaptor(baseUrl);
-        if (orderBookAPI == null) orderBookAPI = new OrderBookAPIAdapter(baseUrl);
+        if (orderBookAPI == null) orderBookAPI = new OrderBookAPIAdapter(baseUrl, apiKey, secret, passPhrase, apiKeyVersion);
         if (historyAPI == null) historyAPI = new HistoryAPIAdapter(baseUrl);
         return new KucoinRestClientImpl(this);
     }
